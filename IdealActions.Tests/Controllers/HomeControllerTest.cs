2e599d596b31905e19c7e3b777d927bb211a2de5
Wade Harvey <harvey007y@gmail.com>
2020-03-01 13:14:03 -0600

    Fix index test

--- IdealActions.Tests/Controllers/HomeControllerTest.cs
+++ IdealActions.Tests/Controllers/HomeControllerTest.cs
@@ -22,7 +22,7 @@ namespace IdealActions.Tests.Controllers
             ViewResult result = controller.Index() as ViewResult;
 
             // Assert
-            Assert.AreEqual("Modify this template to jump-start your ASP.NET MVC application.", result.ViewBag.Message);
+            Assert.AreEqual("Free tool for associating actions with rewards.", result.ViewBag.Message);
         }
 
         [TestMethod]
